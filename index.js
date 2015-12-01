8534eb519867bd1ba3c9595976b91bbb22f6b603
Chris Wood <chris.wood@flightstats.com>
2015-12-01 12:21:41 -0800

    Major release. Promise reject on errors. New hub methods available. Docs updated.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var _ = require('lodash');
+var Promise = require('promise');
 var rp = require('request-promise');
 
 /**
@@ -54,14 +55,20 @@ Datahub.prototype._crud = function (url, method, data) {
  * @param {number} [config.ttlDays=120] - number of days used to limit the items in a channel by time
  * @param {string} [config.description] - channel description
  * @param {array} [config.tags] - string tags
+ * @param {string} [config.replicationSource] - fully qualified path to channel in a another hub
+ * @param {string} [config.storage='SINGLE'] - specification of how to store long term data ('SINGLE' or 'BATCH')
  */
 Datahub.prototype.createChannel = function(config){
+  if (!config){
+    return Promise.reject(new Error('Missing channel configuration'));
+  }
+
   if (!config.name){
-    throw new Error('Missing channel name');
+    return Promise.reject(new Error('Missing channel name'));
   }
 
   if (!config.owner){
-    throw new Error('Missing channel owner');
+    return Promise.reject(new Error('Missing channel owner'));
   }
 
   var data = { name: config.name, owner: config.owner };
@@ -75,8 +82,49 @@ Datahub.prototype.createChannel = function(config){
   }
   if (config.description) { data.description = config.description; }
   if (config.tags) { data.tags = config.tags; }
+  if (config.replicationSource) { data.replicationSource = config.replicationSource; }
+  if (config.storage && (config.storage.toUpperCase() === 'SINGLE' || config.storage.toUpperCase() === 'BATCH')) {
+    data.storage = config.storage.toUpperCase();
+  }
 
   return this._crud(this.config.url + '/channel', 'POST', data);
+};
+
+/**
+ * update a channel
+ * @see {@link https://github.com/flightstats/hub#update-a-channel|Update a Channel}
+ * @param {string} name - channel name
+ * @param {Object} config - updated configuration details for channel
+ * @param {string} [config.owner] - owner of channel
+ * @param {number} [config.ttlDays=120] - number of days used to limit the items in a channel by time
+ * @param {string} [config.description] - channel description
+ * @param {array} [config.tags] - string tags
+ * @param {string} [config.replicationSource] - fully qualified path to channel in a another hub
+ */
+Datahub.prototype.updateChannel = function(name, config){
+  if (!name){
+    return Promise.reject(new Error('Missing channel name'));
+  }
+
+  if (!config){
+    return Promise.reject(new Error('Missing channel configuration'));
+  }
+
+  var data = {};
+  if (config.owner) { data.owner = config.owner; }
+  if (_.isNumber(config.ttlDays)) {
+    data.ttlDays = config.ttlDays;
+  } else if (_.isString(config.ttlDays)){
+    var ttlDays = parseInt(config.ttlDays, 10);
+    if (ttlDays){
+      data.ttlDays = ttlDays;
+    }
+  }
+  if (config.description) { data.description = config.description; }
+  if (config.tags) { data.tags = config.tags; }
+  if (config.replicationSource) { data.replicationSource = config.replicationSource; }
+
+  return this._crud(this.config.url + '/channel/' + name, 'PUT', data);
 };
 
 /**
@@ -94,7 +142,7 @@ Datahub.prototype.getChannels = function(){
  */
 Datahub.prototype.getChannel = function(name){
   if (!name){
-    throw new Error('Missing channel name');
+    return Promise.reject(new Error('Missing channel name'));
   }
 
   return this._crud(this.config.url + '/channel/' + name, 'GET');
@@ -107,10 +155,23 @@ Datahub.prototype.getChannel = function(name){
  */
 Datahub.prototype.deleteChannel = function(name){
   if (!name){
-    throw new Error('Missing channel name');
+    return Promise.reject(new Error('Missing channel name'));
   }
 
   return this._crud(this.config.url + '/channel/' + name, 'DELETE');
+};
+
+/**
+ * get channel status
+ * @see {@link https://github.com/flightstats/hub#channel-status|Get Channel Status}
+ * @param {string} name - channel name
+ */
+Datahub.prototype.getChannelStatus = function(name){
+  if (!name){
+    return Promise.reject(new Error('Missing channel name'));
+  }
+
+  return this._crud(this.config.url + '/channel/' + name + '/status', 'GET');
 };
 
 /**
@@ -121,11 +182,11 @@ Datahub.prototype.deleteChannel = function(name){
  */
 Datahub.prototype.addContent = function(name, content){
   if (!name){
-    throw new Error('Missing channel name');
+    return Promise.reject(new Error('Missing channel name'));
   }
 
   if (!content){
-    throw new Error('Missing content');
+    return Promise.reject(new Error('Missing content'));
   }
 
   return this._crud(this.config.url + '/channel/' + name, 'POST', content);
@@ -139,53 +200,65 @@ Datahub.prototype.addContent = function(name, content){
  */
 Datahub.prototype.getContent = function(name, id){
   if (!name){
-    throw new Error('Missing channel name');
+    return Promise.reject(new Error('Missing channel name'));
   }
 
   if (!id){
-    throw new Error('Missing content id');
+    return Promise.reject(new Error('Missing content id'));
   }
 
   return this._crud(this.config.url + '/channel/' + name + '/' + id, 'GET');
 };
 
 /**
- * get channel status
- * @see {@link https://github.com/flightstats/hub#channel-status|Get Channel Status}
- * @param {string} name - channel name
- */
-Datahub.prototype.getStatus = function(name){
-  if (!name){
-    throw new Error('Missing channel name');
-  }
-
-  return this._crud(this.config.url + '/channel/' + name + '/status', 'GET');
-};
-
-/**
  * get latest channel content
- * @see {@link https://github.com/flightstats/hub#latest-channel-item|Latest Channel Item}
+ * @see {@link https://github.com/flightstats/hub#fetch-latest-channel-item|Latest Channel Item}
  * @param {string} name - channel name
+ * @param {number} [N] - retrieve the latest N items
  */
-Datahub.prototype.getLatest = function(name){
+Datahub.prototype.getLatest = function(name, N){
   if (!name){
-    throw new Error('Missing channel name');
+    return Promise.reject(new Error('Missing channel name'));
   }
 
-  return this._crud(this.config.url + '/channel/' + name + '/latest', 'GET');
+  if (N && !_.isNumber(N)) {
+    return Promise.reject(new Error('Invalid number parameter'));
+  }
+
+  return this._crud(this.config.url + '/channel/' + name + '/latest' +
+    ((N) ? '/' + N : ''), 'GET');
 };
 
 /**
  * get earliest channel content
- * @see {@link https://github.com/flightstats/hub#earliest-channel-item|Earliest Channel Item}
+ * @see {@link https://github.com/flightstats/hub#fetch-earliest-channel-item|Earliest Channel Item}
  * @param {string} name - channel name
+ * @param {number} [N] - retrieve the earliest N items
  */
-Datahub.prototype.getEarliest = function(name){
+Datahub.prototype.getEarliest = function(name, N){
   if (!name){
-    throw new Error('Missing channel name');
+    return Promise.reject(new Error('Missing channel name'));
   }
 
-  return this._crud(this.config.url + '/channel/' + name + '/earliest', 'GET');
+  if (N && !_.isNumber(N)) {
+    return Promise.reject(new Error('Invalid number parameter'));
+  }
+
+  return this._crud(this.config.url + '/channel/' + name + '/earliest' +
+    ((N) ? '/' + N : ''), 'GET');
+};
+
+/**
+ * get channel time
+ * @see {@link https://github.com/flightstats/hub#time-interface|Channel Time}
+ * @param {string} name - channel name
+ */
+Datahub.prototype.getTime = function(name){
+  if (!name){
+    return Promise.reject(new Error('Missing channel name'));
+  }
+
+  return this._crud(this.config.url + '/channel/' + name + '/time', 'GET');
 };
 
 /**
@@ -197,18 +270,27 @@ Datahub.prototype.getEarliest = function(name){
  * @param {string} config.callbackUrl - the fully qualified location to receive callbacks from the server
  * @param {number} [config.parallelCalls=1] - number of callbacks to make in parallel
  * @param {string} [config.startItem] - fully qualified item location where the callback should start from
+ * @param {boolean} [config.paused=false] - When true, this will pause a group callback
+ * @param {string} [config.batch='SINGLE'] - 'SINGLE' will return each item by itself, 'MINUTE' will return each minute's worth of data in the channel
+ * @param {boolean} [config.heartbeat=false] - false for SINGLE, or true for MINUTE batches that will always have a heartbeat, a callback which identifies the end of a minute period
+ * @param {number} [config.maxWaitMinutes=1] - the maximum amount of time between retry attempts to the callbackUrl
+ * @param {number} [config.ttlMinutes=0] - if greater than 0, the hub will not attempt to send an item which is older than the ttl
  */
 Datahub.prototype.createGroupCallback = function(config){
+  if (!config){
+    return Promise.reject(new Error('Missing group configuration'));
+  }
+
   if (!config.name){
-    throw new Error('Missing group name');
+    return Promise.reject(new Error('Missing group name'));
   }
 
   if (!config.channelName){
-    throw new Error('Missing channel name');
+    return Promise.reject(new Error('Missing channel name'));
   }
 
   if (!config.callbackUrl){
-    throw new Error('Missing callback URL');
+    return Promise.reject(new Error('Missing callback URL'));
   }
 
   var data = {
@@ -226,8 +308,97 @@ Datahub.prototype.createGroupCallback = function(config){
   }
 
   if (config.startItem) { data.startItem = config.startItem; }
+  if (_.isBoolean(config.paused) && config.paused) {
+    data.paused = true;
+  }
+  if (config.batch && (config.batch.toUpperCase() === 'SINGLE' || config.batch.toUpperCase() === 'MINUTE')) {
+    data.batch = config.batch.toUpperCase();
+  }
+  if (_.isBoolean(config.heartbeat) && config.heartbeat) {
+    data.heartbeat = true;
+  }
+
+  if (_.isNumber(config.maxWaitMinutes)) {
+    data.maxWaitMinutes = config.maxWaitMinutes;
+  } else if (_.isString(config.maxWaitMinutes)){
+    var maxWaitMinutes = parseInt(config.maxWaitMinutes, 10);
+    if (maxWaitMinutes){
+      data.maxWaitMinutes = maxWaitMinutes;
+    }
+  }
+
+  if (_.isNumber(config.ttlMinutes)) {
+    data.ttlMinutes = config.ttlMinutes;
+  } else if (_.isString(config.ttlMinutes)){
+    var ttlMinutes = parseInt(config.ttlMinutes, 10);
+    if (ttlMinutes){
+      data.ttlMinutes = ttlMinutes;
+    }
+  }
 
   return this._crud(this.config.url + '/group/' + config.name, 'PUT', data);
+};
+
+/**
+ * update a group callback
+ * @see {@link https://github.com/flightstats/hub#group-callback|Group Callbacks}
+ * @param {string} name - group name
+ * @param {Object} config - updated configuration details for group
+ * @param {string} config.callbackUrl - the fully qualified location to receive callbacks from the server
+ * @param {number} [config.parallelCalls=1] - number of callbacks to make in parallel
+ * @param {boolean} [config.paused=false] - When true, this will pause a group callback
+ * @param {boolean} [config.heartbeat=false] - false for SINGLE, or true for MINUTE batches that will always have a heartbeat, a callback which identifies the end of a minute period
+ * @param {number} [config.maxWaitMinutes=1] - the maximum amount of time between retry attempts to the callbackUrl
+ * @param {number} [config.ttlMinutes=0] - if greater than 0, the hub will not attempt to send an item which is older than the ttl
+ */
+Datahub.prototype.updateGroupCallback = function(name, config){
+  if (!name){
+    return Promise.reject(new Error('Missing group name'));
+  }
+
+  if (!config){
+    return Promise.reject(new Error('Missing group configuration'));
+  }
+
+  var data = {};
+
+  if (config.callbackUrl) { data.callbackUrl = config.callbackUrl; }
+
+  if (_.isNumber(config.parallelCalls)) {
+    data.parallelCalls = config.parallelCalls;
+  } else if (_.isString(config.parallelCalls)){
+    var parallelCalls = parseInt(config.parallelCalls, 10);
+    if (parallelCalls){
+      data.parallelCalls = parallelCalls;
+    }
+  }
+
+  if (_.isBoolean(config.paused) && config.paused) {
+    data.paused = true;
+  }
+  if (_.isBoolean(config.heartbeat) && config.heartbeat) {
+    data.heartbeat = true;
+  }
+
+  if (_.isNumber(config.maxWaitMinutes)) {
+    data.maxWaitMinutes = config.maxWaitMinutes;
+  } else if (_.isString(config.maxWaitMinutes)){
+    var maxWaitMinutes = parseInt(config.maxWaitMinutes, 10);
+    if (maxWaitMinutes){
+      data.maxWaitMinutes = maxWaitMinutes;
+    }
+  }
+
+  if (_.isNumber(config.ttlMinutes)) {
+    data.ttlMinutes = config.ttlMinutes;
+  } else if (_.isString(config.ttlMinutes)){
+    var ttlMinutes = parseInt(config.ttlMinutes, 10);
+    if (ttlMinutes){
+      data.ttlMinutes = ttlMinutes;
+    }
+  }
+
+  return this._crud(this.config.url + '/group/' + name, 'PUT', data);
 };
 
 /**
@@ -245,7 +416,7 @@ Datahub.prototype.getGroupCallbacks = function(){
  */
 Datahub.prototype.getGroupCallback = function(name){
   if (!name){
-    throw new Error('Missing group name');
+    return Promise.reject(new Error('Missing group name'));
   }
 
   return this._crud(this.config.url + '/group/' + name, 'GET');
@@ -258,7 +429,7 @@ Datahub.prototype.getGroupCallback = function(name){
  */
 Datahub.prototype.deleteGroupCallback = function(name){
   if (!name){
-    throw new Error('Missing group name');
+    return Promise.reject(new Error('Missing group name'));
   }
 
   return this._crud(this.config.url + '/group/' + name, 'DELETE');
@@ -271,11 +442,157 @@ Datahub.prototype.deleteGroupCallback = function(name){
  */
 Datahub.prototype.getGroupCallbackContent = function(data) {
   if (!data || !data.uris || !data.uris[0]) {
-    throw new Error('Missing data');
+    return Promise.reject(new Error('Missing data'));
   } else {
     var url = data.uris[0];
     return this._crud(url, 'GET');
   }
+};
+
+/**
+ * Create/update a channel alert
+ * @see {@link https://github.com/flightstats/hub#alerts|Alerts}
+ * @param {string} name - alert name
+ * @param {Object} config - configuration details for channel alert
+ * @param {string} config.source - name of the channel to monitor
+ * @param {string} config.serviceName - user defined end point for the alert
+ * @param {number} config.timeWindowMinutes - period of time to evaluate
+ * @param {string} config.operator - can be '>=', '>', '==', '<', or '<='
+ * @param {number} config.threshold - value to compare
+ */
+Datahub.prototype.channelAlert = function(name, config){
+  if (!name){
+    return Promise.reject(new Error('Missing alert name'));
+  }
+
+  if (!config){
+    return Promise.reject(new Error('Missing alert configuration'));
+  }
+
+  if (!config.source){
+    return Promise.reject(new Error('Missing alert source'));
+  }
+
+  if (!config.serviceName){
+    return Promise.reject(new Error('Missing alert service name'));
+  }
+
+  if (!config.timeWindowMinutes){
+    return Promise.reject(new Error('Missing alert time window'));
+  }
+
+  if (!config.operator){
+    return Promise.reject(new Error('Missing alert operator'));
+  }
+
+  if (!config.threshold){
+    return Promise.reject(new Error('Missing alert threshold'));
+  }
+
+  var data = {
+    source: config.source,
+    serviceName: config.serviceName,
+    type: 'channel'
+  };
+
+  if (_.isNumber(config.timeWindowMinutes)) {
+    data.timeWindowMinutes = config.timeWindowMinutes;
+  } else if (_.isString(config.timeWindowMinutes)){
+    var timeWindowMinutes = parseInt(config.timeWindowMinutes, 10);
+    if (timeWindowMinutes){
+      data.timeWindowMinutes = timeWindowMinutes;
+    }
+  }
+  if (!data.timeWindowMinutes){
+    return Promise.reject(new Error('Invalid alert time window'));
+  }
+
+  if (_.isString(config.operator) &&
+    _.indexOf(['>=', '>', '==', '<', '<='], config.operator) !== -1) {
+    data.operator = config.operator;
+  }
+  else {
+    return Promise.reject(new Error('Invalid alert operator'));
+  }
+
+  if (_.isNumber(config.threshold)) {
+    data.threshold = config.threshold;
+  } else if (_.isString(config.threshold)){
+    var threshold = parseInt(config.threshold, 10);
+    if (threshold){
+      data.threshold = threshold;
+    }
+  }
+  if (!data.threshold){
+    return Promise.reject(new Error('Invalid alert threshold'));
+  }
+
+  return this._crud(this.config.url + '/alert/' + name, 'PUT', data);
+};
+
+/**
+ * Create/update a group alert
+ * @see {@link https://github.com/flightstats/hub#alerts|Alerts}
+ * @param {string} name - alert name
+ * @param {Object} config - configuration details for group alert
+ * @param {string} config.source - name of the group to monitor
+ * @param {string} config.serviceName - user defined end point for the alert
+ * @param {number} config.timeWindowMinutes - period of time to evaluate
+ */
+Datahub.prototype.groupAlert = function(name, config){
+  if (!name){
+    return Promise.reject(new Error('Missing alert name'));
+  }
+
+  if (!config){
+    return Promise.reject(new Error('Missing alert configuration'));
+  }
+
+  if (!config.source){
+    return Promise.reject(new Error('Missing alert source'));
+  }
+
+  if (!config.serviceName){
+    return Promise.reject(new Error('Missing alert service name'));
+  }
+
+  if (!config.timeWindowMinutes){
+    return Promise.reject(new Error('Missing alert time window'));
+  }
+
+  var data = {
+    source: config.source,
+    serviceName: config.serviceName,
+    type: 'group'
+  };
+
+  if (_.isNumber(config.timeWindowMinutes)) {
+    data.timeWindowMinutes = config.timeWindowMinutes;
+  } else if (_.isString(config.timeWindowMinutes)){
+    var timeWindowMinutes = parseInt(config.timeWindowMinutes, 10);
+    if (timeWindowMinutes){
+      data.timeWindowMinutes = timeWindowMinutes;
+    }
+  }
+  if (!data.timeWindowMinutes){
+    return Promise.reject(new Error('Invalid alert time window'));
+  }
+
+  return this._crud(this.config.url + '/alert/' + name, 'PUT', data);
+};
+
+/**
+ * Get channe/group alert status
+ * @see {@link https://github.com/flightstats/hub#channel-alert-status|Channel Alert Status}
+ * @see {@link https://github.com/flightstats/hub#group-alert-status|Channel Group Status}
+ * @param {string} name - alert name
+ */
+Datahub.prototype.alertStatus = function(name){
+  if (!name){
+    return Promise.reject(new Error('Missing alert name'));
+  }
+
+  return this._crud(this.config.url + '/alert/' + name, 'GET');
 };
 
 module.exports = Datahub;
